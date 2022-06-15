13b68be789d2f8a4861fc51d2626c45c03734ade
therdman <taylorzaneh@gmail.com>
2022-06-15 15:20:53 +1200

    Trying to compare

--- RetinalMatch.java
+++ RetinalMatch.java
@@ -10,54 +10,110 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.core.Size;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
+import org.opencv.core.Core.MinMaxLocResult;
+import org.opencv.core.Scalar;
+import java.util.List;
+import java.util.ArrayList;
 
 import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfPoint;
 
 public class RetinalMatch {
     public static void main(String[] args) {
         // load the OpenCV native library
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-        // create and print on screen a 3x3 identity matrix
-        System.out.println("Create a 3x3 identity matrix...");
-        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
-        System.out.println("mat = " + mat.dump());
+        //String location = "RIDB/IM000002_18.jpg";
+        
+        // get input from user for files
+        String location1 = args[0];
+        String location2 = args[1];
 
-        // prepare to convert a RGB image in gray scale
-        String location = "RIDB/IM000002_18.jpg";
-        System.out.print("Convert the image at " + location + " in gray scale... ");
-        // get the jpeg image from the internal resource folder
-        Mat image = Imgcodecs.imread(location);
-        // crop
-        Rect crop = new Rect(180, 0, 1100, 1000);
-        image = new Mat(image, crop);
-        // convert the image in gray scale
-        Imgproc.cvtColor(image, image, Imgproc.COLOR_BGR2GRAY);
+        // get the jpg images from the internal resource folder
+        Mat image1 = Imgcodecs.imread(location1);
+        Mat image2 = Imgcodecs.imread(location2);
 
+        // crop to roughly the eye
+        Rect crop = new Rect(180, 0, 1080, 1000);
+        image1 = new Mat(image1, crop);
+        image2 = new Mat(image2, crop);
+
+        // convert the images to gray scale
+        Imgproc.cvtColor(image1, image1, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.cvtColor(image2, image2, Imgproc.COLOR_BGR2GRAY);
 
         // autocontrast histogram equalisation
-        Mat equalisedImage = new Mat(image.rows(), image.cols(), image.type());
-        Imgproc.equalizeHist(image, equalisedImage);
-        Imgcodecs.imwrite("TestEqualised.jpg", equalisedImage);
+        Imgproc.equalizeHist(image1, image1);
+        Imgcodecs.imwrite("ImageEq1.jpg", image1);
+        Imgproc.equalizeHist(image2, image2);
+        Imgcodecs.imwrite("ImageEq2.jpg", image2);
 
-        //blur
-        Mat blurredImage = new Mat(image.rows(), image.cols(), image.type());
-        Mat result = blurredImage;
-        Imgproc.GaussianBlur(equalisedImage, blurredImage, new Size(7, 7), 1.3);
-        Imgproc.Canny(blurredImage, blurredImage, 40, 40*3, 3, false);
-        Imgcodecs.imwrite("TestBlurred.jpg", blurredImage);
+        //blur images
+        Imgproc.GaussianBlur(image1, image1, new Size(7, 7), 1.3);
+        Imgcodecs.imwrite("ImageBl1.jpg", image1);
+        Imgproc.GaussianBlur(image2, image2, new Size(7, 7), 1.3);
+        Imgcodecs.imwrite("ImageBl2.jpg", image2);
+
+        // edge detection
+        Imgproc.Canny(image1, image1, 45, 45*3, 3, false);
+        Imgcodecs.imwrite("ImageEd1.jpg", image1);
+        Imgproc.Canny(image2, image2, 45, 45*3, 3, false);
+        Imgcodecs.imwrite("ImageEd2.jpg", image2);
 
         // thresholding
-        Imgproc.adaptiveThreshold(blurredImage, result, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 5, 20);
-        Imgcodecs.imwrite("TestThresh1.jpg", result);
+        Imgproc.adaptiveThreshold(image1, image1, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 5, 20);
+        Imgcodecs.imwrite("ImageTh1.jpg", image1);
+        Imgproc.adaptiveThreshold(image2, image2, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 5, 20);
+        Imgcodecs.imwrite("ImageTh2.jpg", image2);
 
         // morpholical operations
-        Mat eroeded = new Mat(image.rows(), image.cols(), image.type());
         int kernelSize = 1;
         int elementType = Imgproc.CV_SHAPE_RECT;
         Mat element = Imgproc.getStructuringElement(elementType, new Size(2 * kernelSize + 1, 2 * kernelSize + 1),
             new Point(kernelSize, kernelSize));
-        Imgproc.erode(result, eroeded, element);
-        Imgcodecs.imwrite("TestEroed2.jpg", eroeded);
+        Imgproc.erode(image1, image1, element);
+        Imgcodecs.imwrite("ImageF1.jpg", image1);
+        Imgproc.erode(image2, image2, element);
+        Imgcodecs.imwrite("ImageF2.jpg", image2);
+
+        // matching
+        float[] range = {0, 256};
+        MatOfFloat histRange = new MatOfFloat(range);
+
+        /*
+        Mat hist1 = new Mat();
+        List<Mat> imageList1 = new ArrayList<>();
+        imageList1.add(image1);
+        Imgproc.calcHist(imageList1, new MatOfInt(0), new Mat(), hist1, new MatOfInt(256), histRange);
+        */
+
+        Mat hist2 = new Mat();
+        List<Mat> imageList2 = new ArrayList<>();
+        imageList2.add(image2);
+        Imgproc.calcHist(imageList2, new MatOfInt(0), new Mat(), hist2, new MatOfInt(10), histRange);
+        Core.normalize(hist2, hist2, 0, 1, Core.NORM_MINMAX);
+
+        Mat imageCropped = new Mat();
+        Mat hist1 = new Mat();
+        List<Mat> imageList1 = new ArrayList<>();
+
+        for(int i = 0; i < 900; i += 180) {
+            for(int j = 0; j < 900; j += 100) {
+                crop = new Rect(i, i+100, j, j+100);
+                System.out.println(i);
+                System.out.println(j);
+                imageCropped = new Mat(image1, crop);
+                imageList1.add(imageCropped);
+                Imgproc.calcHist(imageList1, new MatOfInt(0), new Mat(), hist1, new MatOfInt(10), histRange);
+                Core.normalize(hist1, hist1, 0, 1, Core.NORM_MINMAX);
+                imageList1.clear();
+                System.out.println(Imgproc.compareHist(hist1, hist2, 0));
+            }
+        }
+
+        //Mat histImage = new Mat();
+        //Core.normalize(hist1, hist1, 0, 1, Core.NORM_MINMAX);
+        //System.out.println(Imgproc.compareHist(hist1, hist2, 0));
     }
 }
